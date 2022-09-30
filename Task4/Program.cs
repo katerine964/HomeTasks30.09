41b3efc8e65e89a4e1cc2f4bf105d0dddc6d6111
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-09-30 15:36:13 +0300

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число ");
+int C = Convert.ToInt32(Console.ReadLine());
+if (A>=B && A>=C)
+{
+    Console.WriteLine(A+" наибольшее число");
+}
+else
+{
+    if (B>=A && B>=C)
+    {
+        Console.WriteLine(B+" наибольшее число");
+    }
+
+    else
+    {
+        if (C>=A && C>=B)
+        {
+            Console.WriteLine(C+" наибольшее число");
+        }
+    }
+}
+
