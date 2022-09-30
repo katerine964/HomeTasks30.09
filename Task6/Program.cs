0803f422d24ba96409c821829caad467625f2eab
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-09-30 15:37:58 +0300

    Add files via upload

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2==0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
+
