a5fce4ef9f4de1674d5a65d371e26bb64e7299dc
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-09-30 15:38:42 +0300

    Add files via upload

---
+++ Task8/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=2; i<=N; i=i+2)
+{
+    Console.Write(i + " ");
+}
