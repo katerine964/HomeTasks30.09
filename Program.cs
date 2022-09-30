3eee6775918f3a71089315182da5dc7dbeb4f546
Kate <ekatromanova90@mail.ru>
2022-09-30 15:24:20 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a==b)
+{
+    Console.WriteLine("числа равны");
+}
+if (a>b)
+{
+    Console.WriteLine(a + " наибольшее число " + b + " наименьшее число");
+}
+if (a<b)
+{
+    Console.WriteLine(b + " наибольшее число " + a + " наименьшее число");
+}
